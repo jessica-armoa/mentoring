a1b51c3e5a6f9c209b2361e53fd58d5ebd547f53
Brenda Huemer <brendahuemer@gmail.com>
2023-10-06 16:20:04 -0300

    editar mentor completado crud completo

--- mentoring_app/app/urls.py
+++ mentoring_app/app/urls.py
@@ -9,6 +9,7 @@ urlpatterns = [
     path('register/user', views.register_user),
     path('edit/user/<int:id>', views.edit_user),
     path('register/mentor', views.register_mentor),
+    path('edit/mentor/<int:id>', views.edit_mentor),
     path('guia/mentor', views.guia_mentor),
     path('dashboard', views.dashboard),
     path('validate_calendly_username/', views.validate_calendly_username, name='validate_calendly_username'),
